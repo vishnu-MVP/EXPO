28af8dab84f978e47030b6797ea717ac535cb0f9
vishnu-MVP <92323724+vishnu-MVP@users.noreply.github.com>
2024-04-10 23:52:40 +0530

    completed react basic

--- frontend/froexpo/src/App.js
+++ frontend/froexpo/src/App.js
@@ -5,11 +5,11 @@ function App() {
   return (
     <div className="maincon">
     <div className="container">
-        <h1 >Welcome To Our Expo</h1>
+        <h1 id="ih1">Welcome To Our Expo</h1>
     </div>
     <div className="container">
         <Link to='/ticket'>
-            <button type="submit" value="Get Your Tickets!" className="rainbow rainbow-1"/>
+            <button type="submit" className="rainbow rainbow-1">Get Your Tickets!</button>
             </Link>
    
     </div>
--- frontend/froexpo/src/Login.js
+++ frontend/froexpo/src/Login.js
@@ -1,19 +1,24 @@
 import React from 'react'
 import './Login.css';
-import { Link } from 'react-router-dom'
-
+import { Link,useNavigate} from 'react-router-dom'
 const Login = () => {
+    const nav=useNavigate()
+    const HandleSubmit=(event)=>{
+        event.preventDefault();
+     
+        nav('/home')
+    }
   return (
     <center>
     <div className='parent'>
     <div class="lcontainer">
         <h1>Registration</h1>
-        <Link to='/home'>
-        <form action="home.html">
+        
+        <form onSubmit={HandleSubmit}>
             <table>
                 <tr>
                     <td class="llabel"><label for="Name">Name</label></td>
-                    <td><input type="text" id="name" class="linput" required placeholder="Give Your Name"/></td>
+                    <td><input type="text" id="name" class="linput" name='name' required placeholder="Give Your Name" /></td>
                 </tr>
                 <tr>
                     <td class="llabel"><label for="Email">Email</label></td>
@@ -24,11 +29,11 @@ const Login = () => {
                     <td><input type="number" id="age" class="linput" placeholder="Give Your Age"/></td>
                 </tr>
                 <tr>
-                    <td><button type="submit" class="lsubmit lsubmit-1" value="Register"/></td>
+                    <td><button type="submit" class="lsubmit lsubmit-1" >Register</button></td>
                 </tr>
             </table>
         </form>
-        </Link>
+       
     </div>
     </div>
     </center>
